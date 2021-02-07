55349c0fe9553a5207729b94623eed170e2aa4da
aedias <aedias@gmail.com>
2021-02-07 17:49:16 -0300

    Obtendo Imagem da Galeria

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: 'http://localhost:8080',
+    baseUrlx: 'http://localhost:8080',
+    baseUrl: 'https://curso-spring-ionic-67.herokuapp.com',
     bucketBaseUrl: 'https://curso-spring-ionic-aed.s3-sa-east-1.amazonaws.com'
 }
